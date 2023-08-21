782055be614cc2010f106d9d510fb41e5f2b2c2d
Redoxahmii <fullmetal2334@gmail.com>
2023-08-21 16:12:06 +0500

    animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex w-screen h-screen items-center justify-center">
       <section className="flex w-full max-w-8xl px-5 sm:px-10 lg:px-20">
         <div className="flex md:flex-row flex-col">
-          <div className="w-full md:w-1/2 mt-10">
+          <div className="w-full md:w-1/2 mt-10 animate-fade-right">
             <h1 className="text-7xl font-bold tracking-tighter text-base-content/40">
               Simplify
             </h1>
@@ -30,7 +30,7 @@ export default function Home() {
               </button>
             </div>
           </div>
-          <div className=" md:w-1/2 mt-20 md:mt-0">
+          <div className=" md:w-1/2 mt-20 md:mt-0 animate-fade-up">
             <img
               className="object-cover w-full h-full rounded-xl shadow-xl"
               src="https://images.unsplash.com/photo-1512678080530-7760d81faba6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1174&q=80"
